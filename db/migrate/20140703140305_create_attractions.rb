41bae1ff4c1eb90ac03c2e1ae81f66ddd93d64ce
CodeByLine <yleventhal@ymail.com>
2016-10-04 14:12:15 -0400

    active record relationship set up

--- db/migrate/20140703140305_create_attractions.rb
+++ db/migrate/20140703140305_create_attractions.rb
@@ -1,3 +1,16 @@
 class CreateAttractions < ActiveRecord::Migration
  # Write your migrations here
+
+   def change
+     create_table :attractins do |t|
+       t.string  :name
+       t.integer :user_id
+       t.integer :ride_id
+       t.integer :height
+       t.integer :nausea_rating
+       t.integer :happiness_rating
+       t.integer :tickets
+
+       t.timestamps null: false
+   end
 end
