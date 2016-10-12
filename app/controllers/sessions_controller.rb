bcf6881cb27440edf6fd811bbf5ca7b02b0effb9
CodeByLine <yleventhal@ymail.com>
2016-10-11 21:32:21 -0400

    filled out controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,22 @@
 class SessionsController < ApplicationController
 
-      def new
+    def destroy
+      session[:user_id] = nil
+      redirect_to root_url
+    end
+
+    def new
+      @user = User.new
+      @users = User.all
+    end
+
+    def create
+      @user = User.find_by(name: params[:user][:name])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user), notice: "Welcome back to the theme park!"
+      else
+        redirect_to signin_path
       end
-
-      def create
-        user = User.find_by(name: params[:user][:name])
-
-        if user && user.authenticate(params[:user][:password])
-          session[:user_id] = user.id
-          redirect_to 'users/index'
-        else
-          redirect_to 'users/new'
-        end
-      end
-
-      def destroy
-        session.delete :name
-      end
-
+    end
   end
