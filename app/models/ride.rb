96c9d9fee2054879db292dd27965aeeb11564264
CodeByLine <yleventhal@ymail.com>
2016-10-11 21:12:09 -0400

    renamed static_pages thru-out; reconstruct everything else

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,5 +1,31 @@
 class Ride < ActiveRecord::Base
   # write associations here
   belongs_to :attraction
-  belongs_to :users
+  belongs_to :user
+
+  def take_ride
+    # if user.height < attraction.min_height
+    #   return "Sorry. You are not tall enough to ride the #{attraction.name}."
+    # elsif user.tickets < attraction.tickets
+    #   return "Sorry. You do not have enough tickets to the #{attraction.name}."
+    # elseif
+      if user.height < attraction.min_height && user.tickets < attraction.tickets
+        return "Sorry. You do not have enough tickets the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+      elsif user.height < attraction.min_height
+          return "Sorry. You are not tall enough to ride the #{attraction.name}."
+      elsif user.tickets < attraction.tickets
+          return "Sorry. You do not have enough tickets the #{attraction.name}."
+      end
+
+      tickets_remaining =  user.tickets - attraction.tickets
+      nausea_stats =   user.nausea + attraction.nausea_rating
+      happiness_stats = user.happiness + attraction.happiness_rating
+      user.update(:tickets => tickets_remaining, :nausea => nausea_stats, :happiness => happiness_stats)
+  end
+
+  def tickets?
+      tickets_remaining =
+      user.update
+  end
+
 end
