8a824d6f3e02a0e6e4a1d1a9c1df6fe35a85cffc
CodeByLine <yleventhal@ymail.com>
2016-10-20 19:00:27 -0400

    27 errors left

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,71 @@
+class AttractionsController < ApplicationController
+    before_action :set_attraction, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @attractions = Attraction.all
+    end
+
+    def show
+      # @ride = Ride.new
+      @user = @current_user
+    end
+
+    def new
+      # if @user.role > 0
+        @attraction = Attraction.new
+      # else
+      #   "You don't have the privilege to create attractions."
+      #   redirect_to user_path
+      # end
+    end
+
+    def create
+      @attraction = Attraction.new(attraction_params)
+      # respond_to do |format|
+        if @attraction.save
+          redirect_to @attraction, notice: 'Attraction was successfully created.'
+          # format.html { redirect_to @attraction, notice: 'Attraction was successfully created.' }
+        else
+          render "new"
+          # format.html { render :new }
+        end
+    end
+
+
+    def edit
+    end
+
+    def update
+      # respond_to do |format|
+        if @attraction.update(attraction_params)
+          redirect_to @attraction, notice: 'Attraction was successfully updated.'
+          # format.html { redirect_to @attraction, notice: 'Attraction was successfully updated.' }
+        else
+          render :edit
+          # format.html { render :edit }
+        end
+    end
+
+    def destroy
+      @attraction.destroy
+      redirect_to attractions_url, notice: 'Attraction was successfully destroyed.'
+      # respond_to do |format|
+        # format.html { redirect_to attractions_url, notice: 'Attraction was successfully destroyed.' }
+    end
+
+  private
+    def set_attraction
+      @attraction = Attraction.find(params[:id])
+    end
+
+    def attraction_params
+      params.require(:attraction).permit(
+        :name,
+        :min_height,
+        :tickets,
+        :happiness_rating,
+        :nausea_rating
+      )
+    end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
   # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
+
+  enum role: [:user, :admin]
+
+  def mood
+     if self.happiness && self.nausea
+       mood = self.happiness - self.nausea
+       mood > 0 ? "happy" : "sad"
+     end
+   end
+
+   def num_of_rides
+     self.attractions.count
+   end
+
+
 end
